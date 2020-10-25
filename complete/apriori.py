83e1c5564e47fbe61f9d21a101ec6c83896543fc
Tony Chou <tcheon8788@gmail.com>
2020-10-25 20:22:10 +0800

    modifying readme

--- complete/apriori.py
+++ complete/apriori.py
@@ -1,6 +1,7 @@
 from csv import reader
 from collections import defaultdict
 from itertools import chain, combinations
+from optparse import OptionParser
 
 def dataToCSV(fname):
     first = True
@@ -79,19 +80,24 @@ def associationRule(freqItemSet, itemSetWithSup, minConf):
                     rules.append([set(s), set(item.difference(s)), confidence])
     return rules
 
-if __name__ == "__main__":
-    fname = 'tesco2'
-    # dataToCSV(fname)
-    C1ItemSet, itemSetList = getFromFile(fname + '.csv')
+def getItemSetFromList(itemSetList):
+    tempItemSet = set()
 
+    for itemSet in itemSetList:
+        for item in itemSet:
+            tempItemSet.add(frozenset([item]))
+
+    return tempItemSet
+
+def apriori(itemSetList, minSup, minConf):
+    C1ItemSet = getItemSetFromList(itemSetList)
     # Final result global frequent itemset
     globalFreqItemSet = dict()
     # Storing global itemset with support count
     globalItemSetWithSup = defaultdict(int)
-    minSup = 0.5
-    minConf = 0.5
 
-    L1ItemSet = getAboveMinSup(C1ItemSet, itemSetList, minSup, globalItemSetWithSup)
+    L1ItemSet = getAboveMinSup(
+        C1ItemSet, itemSetList, minSup, globalItemSetWithSup)
     currentLSet = L1ItemSet
     k = 2
 
@@ -104,17 +110,63 @@ if __name__ == "__main__":
         # Perform subset testing and remove pruned supersets
         candidateSet = pruning(candidateSet, currentLSet, k-1)
         # Scanning itemSet for counting support
-        currentLSet = getAboveMinSup(candidateSet, itemSetList, minSup, globalItemSetWithSup)
+        currentLSet = getAboveMinSup(
+            candidateSet, itemSetList, minSup, globalItemSetWithSup)
         k += 1
 
-    print('Frequent patterns:')
-    for k, itemSetList in globalFreqItemSet.items():
-        for itemSet in itemSetList:
-            print(set(itemSet))
-    
-    # Generating rule
-    print('rules:')
     rules = associationRule(globalFreqItemSet, globalItemSetWithSup, minConf)
     rules.sort(key=lambda x: x[2])
-    for rule in rules:
-        print('{} ==> {}   {:.3f}'.format(rule[0], rule[1], rule[2]))
+
+    return globalFreqItemSet, rules
+
+def aprioriFromFile(fname, minSup, minConf):
+    C1ItemSet, itemSetList = getFromFile(fname)
+
+    # Final result global frequent itemset
+    globalFreqItemSet = dict()
+    # Storing global itemset with support count
+    globalItemSetWithSup = defaultdict(int)
+
+    L1ItemSet = getAboveMinSup(
+        C1ItemSet, itemSetList, minSup, globalItemSetWithSup)
+    currentLSet = L1ItemSet
+    k = 2
+
+    # Calculating frequent item set
+    while(currentLSet):
+        # Storing frequent itemset
+        globalFreqItemSet[k-1] = currentLSet
+        # Self-joining Lk
+        candidateSet = getUnion(currentLSet, k)
+        # Perform subset testing and remove pruned supersets
+        candidateSet = pruning(candidateSet, currentLSet, k-1)
+        # Scanning itemSet for counting support
+        currentLSet = getAboveMinSup(
+            candidateSet, itemSetList, minSup, globalItemSetWithSup)
+        k += 1
+
+    rules = associationRule(globalFreqItemSet, globalItemSetWithSup, minConf)
+    rules.sort(key=lambda x: x[2])
+
+    return globalFreqItemSet, rules
+
+if __name__ == "__main__":
+    optparser = OptionParser()
+    optparser.add_option('-f', '--inputFile',
+                         dest='inputFile',
+                         help='CSV filename',
+                         default=None)
+    optparser.add_option('-s', '--minSupport',
+                         dest='minSup',
+                         help='Min support (float)',
+                         default=0.5,
+                         type='float')
+    optparser.add_option('-c', '--minConfidence',
+                         dest='minConf',
+                         help='Min confidence (float)',
+                         default=0.5,
+                         type='float')
+
+    (options, args) = optparser.parse_args()
+
+    freqItemSet, rules = aprioriFromFile(options.inputFile, options.minSup, options.minConf)
